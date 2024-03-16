77a5df5e4ee175e50d89707b06a905e684ced25f
IrjiSyahrul <irjisyahrukchannel@gmail.com>
2024-03-16 12:18:38 +0700

    push dari branch-class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿internal class Program
 {
+
+class DataGeneric<T>
+    {
+        public T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan : " + Data);
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataGeneric<string> dataString = new DataGeneric<string>("Ini adalah data rahasia kode nuklir");
+        dataString.PrintData();
+        DataGeneric<int> dataInt = new DataGeneric<int> (1302220088);
+        dataInt.PrintData();
     }
 }
