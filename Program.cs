7fe65a2134758b1b5ee51a7f6d4fc4d7c9209443
IrjiSyahrul <irjisyahrukchannel@gmail.com>
2024-03-16 12:02:22 +0700

    push dari branch generic-method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿internal class Program
 {
+    class HaloGeneric<T> 
+    {
+        public void SapaUser(T user)
+        {
+            Console.WriteLine("Halo user" + user);
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       HaloGeneric<string> haloGeneric = new HaloGeneric<string>();
+        haloGeneric.SapaUser("UCOK");
+
+        HaloGeneric<int> haloGenericINT = new HaloGeneric<int>();
+        haloGenericINT.SapaUser(007);
     }
 }
